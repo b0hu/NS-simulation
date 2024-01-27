8dc793b9d695f63af02c14fb48a0f8e2c5c90705
= <=>
2024-01-27 18:13:31 +0800

    modify

--- mygym.h
+++ mygym.h
@@ -1,26 +1,37 @@
 #ifndef MY_GYM_ENTITY_H
 #define MY_GYM_ENTITY_H
 
-//#include "ns3/opengym-module.h"
+#include "ns3/opengym-module.h"
 #include "ns3/net-device-container.h"
 #include "ns3/flow-monitor-module.h"
 #include "ns3/nr-mac-scheduler-tdma.h"
 #include "ns3/nr-mac-scheduler-ns3.h"
 #include "ns3/nr-mac-scheduler-ofdma.h"
+#include "ns3/log.h"
 
 namespace ns3{
     extern NetDeviceContainer enbNetDev;
     // extern NetDeviceContainer ueNetDev;
-    extern NetDeviceContainer eMBBNetDev;
-    extern NetDeviceContainer mMTCNetDev;
-    extern NetDeviceContainer URLLCNetDev;
+    extern NetDeviceContainer eMBBNetDev1;
+    extern NetDeviceContainer mMTCNetDev1;
+    extern NetDeviceContainer URLLCNetDev1;
+
+    extern NetDeviceContainer eMBBNetDev2;
+    extern NetDeviceContainer mMTCNetDev2;
+    extern NetDeviceContainer URLLCNetDev2;
 
     extern NodeContainer gNbNodes;
     // extern NodeContainer ueNodes;
-    extern NodeContainer eMBBueNodes;
-    extern NodeContainer mMTCueNodes;
-    extern NodeContainer URLLCueNodes;
+    extern NodeContainer eMBBueNodes1;
+    extern NodeContainer mMTCueNodes1;
+    extern NodeContainer URLLCueNodes1;
+
+    extern NodeContainer eMBBueNodes2;
+    extern NodeContainer mMTCueNodes2;
+    extern NodeContainer URLLCueNodes2;
     // extern GridScenarioHelper gridScenario;
+
+    extern std::unordered_map<int, float> ue_metric;
 
     extern uint64_t randomStream;
     
@@ -29,19 +40,27 @@ namespace ns3{
     extern double gNbHeight;
     extern double ueHeight;
 
-    extern uint32_t embbPacketSize;
-    extern uint32_t mmtcPacketSize;
-    extern uint32_t urllcPacketSize;
+    extern uint32_t embbPacketSize1;
+    extern uint32_t mmtcPacketSize1;
+    extern uint32_t urllcPacketSize1;
+
+    extern uint32_t embbPacketSize2;
+    extern uint32_t mmtcPacketSize2;
+    extern uint32_t urllcPacketSize2;
 
     extern uint32_t simTimeMs;
     extern uint32_t envStepTime;
-    extern uint32_t eMBBStartTimeMs;
+    extern uint32_t StartTimeMs;
     extern double centralFrequencyBand;
     extern double bandwidthBand1 ;
     //double totalTxPower = 4;
-    extern uint16_t PorteMBB;
-    extern uint16_t PortmMTC;
-    extern uint16_t PortURLLC;
+    extern uint16_t PorteMBB1;
+    extern uint16_t PortmMTC1;
+    extern uint16_t PortURLLC1;
+
+    extern uint16_t PorteMBB2;
+    extern uint16_t PortmMTC2;
+    extern uint16_t PortURLLC2;
 
     extern uint32_t simSeed;
     extern uint32_t testArg;
@@ -49,11 +68,39 @@ namespace ns3{
     extern double averageFlowThroughput;
     extern double averageFlowDelay;
 
+    extern double embbthruput;
+    extern double embbdelay;
+
+    extern double urllcthruput;
+    extern double urllcdelay;
+    extern double urllcreliability; 
+
+    extern double mmtcthruput;
+    extern double mmtcdelay;
+
     extern FlowMonitorHelper flowmonHelper;
     extern Ptr<ns3::FlowMonitor> monitor;
     extern NodeContainer endpointNodes;
 
+    extern Ptr<OpenGymInterface> openGym;
+    extern std::ofstream myfile;
+    extern std::ofstream embbfile;
+    extern std::ofstream urllcfile;
+    extern std::ofstream mmtcfile;
+    extern std::ofstream rewardfile;
+
     void flow_monitor();
+    void reset_flow();
+    void save_data();
+    // static TypeId GetTypeId (void);
+    Ptr<OpenGymSpace> GetActionSpace();
+    Ptr<OpenGymSpace> GetObservationSpace();
+    Ptr<OpenGymDataContainer> GetObservation();
+    float GetReward(void);
+    bool GetGameOver(void);
+    bool ExecuteActions(Ptr<OpenGymDataContainer> action);
+    std::string GetExtraInfo(void);
+    void ScheduleNextStateRead();
 
     // class MyGym : public OpenGymEnv
     // {
@@ -72,73 +119,18 @@ namespace ns3{
     //     std::string GetExtraInfo(void);
     // };
 
-    
-    /*class CustomScheduler :  public NrMacSchedulerNs3
-    {
-        public:
-            CustomScheduler();
-            ~CustomScheduler() override
-            {}
-            static TypeId GetTypeId();
-            //struct PoInFTPlane ft = new PointInFTPlane(rbg,sym);
 
-        private:
-        std::shared_ptr<NrMacSchedulerUeInfo> CreateUeRepresentation(
-        const NrMacCschedSapProvider::CschedUeConfigReqParameters& params) const override;
-
-        uint8_t GetTpc() const override;
-        
-        BeamSymbolMap AssignDLRBG(uint32_t symAvail, const ActiveUeMap& activeDl) const override;
-        
-        BeamSymbolMap AssignULRBG(uint32_t symAvail, const ActiveUeMap& activeUl) const override;
-
-        std::shared_ptr<DciInfoElementTdma> CreateDlDci(
-        PointInFTPlane* spoint,
-        const std::shared_ptr<NrMacSchedulerUeInfo>& ueInfo,
-        uint32_t maxSym) const override;
-
-        std::shared_ptr<DciInfoElementTdma> CreateUlDci(
-        PointInFTPlane* spoint,
-        const std::shared_ptr<NrMacSchedulerUeInfo>& ueInfo,
-        uint32_t maxSym) const override;
-
-        void ChangeDlBeam(PointInFTPlane* spoint, uint32_t symOfBeam) const override
-        {}
-
-        void ChangeUlBeam(PointInFTPlane* spoint, uint32_t symOfBeam) const override
-        {}
-
-    };*/
-
-    class CustomScheduler : public NrMacSchedulerOfdma
+    class CustomScheduler : public NrMacSchedulerNs3
     {
     public:
-        /**
-         * \brief GetTypeId
-         * \return The TypeId of the class
-         */
+      
         static TypeId GetTypeId();
 
-        /**
-         * \brief NrMacSchedulerTdmaRR constructor
-         */
         CustomScheduler();
-
-        /**
-         * \brief ~NrMacSchedulerTdmaRR deconstructor
-         */
+        
         ~CustomScheduler() override
         {
         }
-	/*class Test :
-	    {	
-    		public:
-		Test(){}
-		~Test(){}
-		void hello(){
-			cout << "hello" << endl;
-		}
-		};*/
 
     protected:
 	        /**
@@ -155,7 +147,7 @@ namespace ns3{
          */
         std::function<bool(const NrMacSchedulerNs3::UePtrAndBufferReq& lhs,
                         const NrMacSchedulerNs3::UePtrAndBufferReq& rhs)>
-        GetUeCompareDlFn() const override;
+        GetUeCompareDlFn() const;
 
         /**
          * \brief Return the comparison function to sort UL UE according to the scheduler policy
@@ -163,7 +155,7 @@ namespace ns3{
          */
         std::function<bool(const NrMacSchedulerNs3::UePtrAndBufferReq& lhs,
                         const NrMacSchedulerNs3::UePtrAndBufferReq& rhs)>
-        GetUeCompareUlFn() const override;
+        GetUeCompareUlFn() const;
 
         /**
          * \brief Update the UE representation after a symbol (DL) has been assigned to it
@@ -175,7 +167,7 @@ namespace ns3{
          */
         void AssignedDlResources(const UePtrAndBufferReq& ue,
                                 const FTResources& assigned,
-                                const FTResources& totAssigned) const override;
+                                const FTResources& totAssigned) const;
 
         /**
          * \brief Update the UE representation after a symbol (DL) has been assigned to it
@@ -187,31 +179,108 @@ namespace ns3{
          */
         void AssignedUlResources(const UePtrAndBufferReq& ue,
                                 const FTResources& assigned,
-                                const FTResources& totAssigned) const override;
+                                const FTResources& totAssigned) const;
 
         // RR is a simple scheduler: it doesn't do anything in the next
         // inherited calls.
         void NotAssignedDlResources(const UePtrAndBufferReq& ue,
                                     const FTResources& notAssigned,
-                                    const FTResources& totalAssigned) const override
+                                    const FTResources& totalAssigned) const
         {
         }
 
         void NotAssignedUlResources(const UePtrAndBufferReq& ue,
                                     const FTResources& notAssigned,
-                                    const FTResources& totalAssigned) const override
+                                    const FTResources& totalAssigned) const
         {
         }
 
         void BeforeDlSched(const UePtrAndBufferReq& ue,
-                        const FTResources& assignableInIteration) const override
+                        const FTResources& assignableInIteration) const
         {
         }
 
         void BeforeUlSched(const UePtrAndBufferReq& ue,
-                        const FTResources& assignableInIteration) const override
+                        const FTResources& assignableInIteration) const
         {
         }
+
+        // Advanced part
+        BeamSymbolMap AssignDLRBG(uint32_t symAvail, const ActiveUeMap& activeDl) const override;
+        BeamSymbolMap AssignULRBG(uint32_t symAvail, const ActiveUeMap& activeUl) const override;
+
+        std::shared_ptr<DciInfoElementTdma> CreateDlDci(
+            PointInFTPlane* spoint,
+            const std::shared_ptr<NrMacSchedulerUeInfo>& ueInfo,
+            uint32_t maxSym) const override;
+        std::shared_ptr<DciInfoElementTdma> CreateUlDci(
+            PointInFTPlane* spoint,
+            const std::shared_ptr<NrMacSchedulerUeInfo>& ueInfo,
+            uint32_t maxSym) const override;
+
+        /**
+         * \brief Advance the starting point by the number of symbols specified,
+         * resetting the RB count to 0
+         * \param spoint Starting point
+         * \param symOfBeam Number of symbols for the beam
+         */
+        void ChangeDlBeam(PointInFTPlane* spoint, uint32_t symOfBeam) const override;
+
+        void ChangeUlBeam(PointInFTPlane* spoint, uint32_t symOfBeam) const override;
+
+        CustomScheduler::BeamSymbolMap GetSymPerBeam(uint32_t symAvail,
+                                                        const ActiveUeMap& activeDl) const;
+
+        uint8_t GetTpc() const override;
+
+    private:
+        TracedValue<uint32_t> m_tracedValueSymPerBeam;
+        static std::vector<UePtrAndBufferReq> GetUeVectorFromActiveUeMap(const ActiveUeMap& activeUes);
+        typedef std::function<void(const UePtrAndBufferReq&, const FTResources&)>
+        BeforeSchedFn; //!< Before scheduling function
+        /**
+         * \brief //!< Function to notify a successfull assignment
+         */
+        typedef std::function<void(const UePtrAndBufferReq&, const FTResources&, const FTResources&)>
+            AfterSuccessfullAssignmentFn;
+        /**
+         * \brief Function to notify that the UE did not get any resource in one iteration
+         */
+        typedef std::function<void(const UePtrAndBufferReq&, const FTResources&, const FTResources&)>
+            AfterUnsucessfullAssignmentFn;
+        typedef std::function<uint32_t&(const UePtr& ue)> GetRBGFn; //!< Getter for the RBG of an UE
+        typedef std::function<uint32_t(const UePtr& ue)> GetTBSFn;  //!< Getter for the TBS of an UE
+        typedef std::function<uint8_t&(const UePtr& ue)>
+            GetSymFn; //!< Getter for the number of symbols of an UE
+        typedef std::function<bool(const NrMacSchedulerNs3::UePtrAndBufferReq& lhs,
+                                const NrMacSchedulerNs3::UePtrAndBufferReq& rhs)>
+            CompareUeFn;
+        typedef std::function<CompareUeFn()> GetCompareUeFn;
+
+        // BeamSymbolMap AssignRBGTDMA(
+        //     uint32_t symAvail,
+        //     const ActiveUeMap& activeUe,
+        //     const std::string& type,
+        //     const BeforeSchedFn& BeforeSchedFn,
+        //     const GetCompareUeFn& GetCompareFn,
+        //     const GetTBSFn& GetTBSFn,
+        //     const GetRBGFn& GetRBGFn,
+        //     const GetSymFn& GetSymFn,
+        //     const AfterSuccessfullAssignmentFn& SuccessfullAssignmentFn,
+        //     const AfterUnsucessfullAssignmentFn& UnSuccessfullAssignmentFn) const;
+
+        std::shared_ptr<DciInfoElementTdma> CreateDci(
+            PointInFTPlane* spoint,
+            const std::shared_ptr<NrMacSchedulerUeInfo>& ueInfo,
+            std::vector<uint32_t> tbs,
+            DciInfoElementTdma::DciFormat fmt,
+            std::vector<uint8_t> mcs,
+            std::vector<uint8_t> ndi,
+            std::vector<uint8_t> rv,
+            uint8_t numSym) const;
+        
+        float default_metric{0.0};
+        
     };
 
     class CustomSchedulerUeInfo : public NrMacSchedulerUeInfo
@@ -244,7 +313,8 @@ namespace ns3{
         static bool CompareUeWeightsDl(const NrMacSchedulerNs3::UePtrAndBufferReq& lue,
                                     const NrMacSchedulerNs3::UePtrAndBufferReq& rue)
         {
-            return (lue.first->m_dlRBG < rue.first->m_dlRBG);
+            // return (lue.first->m_dlRBG < rue.first->m_dlRBG);
+            return (ue_metric[lue.first->m_rnti] < ue_metric[rue.first->m_rnti]);
         }
 
         /**
@@ -265,6 +335,7 @@ namespace ns3{
         {
             return (lue.first->m_ulRBG < rue.first->m_ulRBG);
         }
+
     };
 
 }
--- test.py
+++ test.py
@@ -4,7 +4,14 @@
 import argparse
 from ns3gym import ns3env
 import gym
+
 import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
 
 __author__ = "Piotr Gawlowicz"
 __copyright__ = "Copyright (c) 2018, Technische Universität Berlin"
@@ -25,18 +32,39 @@ args = parser.parse_args()
 startSim = bool(args.start)
 iterationNum = int(args.iterations)
 
-port =1234 
+port =1111
 simTime = 2000 # seconds
 stepTime = 200  # seconds
 seed = 0
 simArgs = {"--simTime": simTime,
            "--testArg": 123}
 debug = False
+rewards = []
+returns = []
+values = []
+gamma = 0.85
+td_target = 0
+
+#values: predicted rewards by value network
+#returns: calculated discounted returns
+#advantages: returns - values
+#advantages = sum of future discounted rewards
 
 env = ns3env.Ns3Env(port=port, stepTime=stepTime, startSim=startSim, simSeed=seed, simArgs=simArgs, debug=debug)
+# env = gym.make('ns3-v0')
 # simpler:
 #env = ns3env.Ns3Env()
 env.reset()
+
+# set up matplotlib
+is_ipython = 'inline' in matplotlib.get_backend()
+if is_ipython:
+    from IPython import display
+
+plt.ion()
+
+# if GPU is to be used
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 ob_space = env.observation_space
 ac_space = env.action_space
@@ -45,6 +73,134 @@ print("Action space: ", ac_space, ac_space.dtype)
 
 stepIdx = 0
 currIt = 0
+total_episodes = 200
+
+'''class DeepIO(nn.Module):
+    def __init__(self):
+        super(DeepIO, self).__init__()
+        self.rnn = nn.LSTM(input_size=6, hidden_size=512,
+                           num_layers=2, bidirectional=True)
+        self.drop_out = nn.Dropout(0.25)
+        self.fc1 = nn.Linear(512, 256)
+        self.bn1 = nn.BatchNorm1d(256)
+        self.fc_out = nn.Linear(256, 7)
+
+    def forward(self, x):
+        """
+        args:
+        x:  a list of inputs of diemension [BxTx6]
+        """
+        outputs = []
+        # iterate in the batch through all sequences
+        for xx in x:
+            s, n = xx.shape
+            out, hiden = self.rnn(xx.unsqueeze(1))
+            out = out.view(s, 1, 2, 512)
+            out = out[-1, :, 0]
+            outputs.append(out.squeeze())
+        outputs = torch.stack(outputs)
+
+        y = F.relu(self.fc1(outputs), inplace=True)
+        y = self.bn1(y)
+        y = self.drop_out(y)
+        y = self.out(y)
+        return y
+
+def edit_data(data):
+    data = torch.tensor(data, dtype=torch.float)
+    data = torch.reshape(data, (data.shape[0],1))
+    return data'''
+
+#design a LStm Network as the actor network
+class ActorNetwork(nn.Module):
+
+    def __init__(self,input_size,hidden_size,action_size):
+        super(ActorNetwork, self).__init__()
+        self.input_size = input_size
+        self.hidden_size = hidden_size
+        self.action_size = action_size
+        self.rnn1 = nn.LSTM(self.input_size, self.hidden_size, self.action_size, bidirectional=True)
+        self.rnn2 = nn.LSTM(2, self.hidden_size, self.action_size, bidirectional=False)
+
+    def forward(self,x):
+        x = x.cuda()
+        x, (_, _) = self.rnn1(x)
+        x, (_, _) = self.rnn2(x)
+        #attention mechanism
+        y = F.softmax(x, dim = 0)
+        x = torch.div(x,y)
+        return x
+    
+actor = ActorNetwork(1, 1, 1)
+actor = actor.to(device)
+    
+class ValueNetwork(nn.Module):
+
+    def __init__(self,input_size,hidden_size,action_size):
+        super(ValueNetwork, self).__init__()
+        self.input_size = input_size
+        self.hidden_size = hidden_size
+        self.action_size = action_size
+        self.rnn1 = nn.LSTM(self.input_size, self.hidden_size, self.action_size, bidirectional=True)
+        self.rnn2 = nn.LSTM(2, self.hidden_size, self.action_size, bidirectional=False)
+        self.rnn3 = nn.LSTM(self.input_size, self.hidden_size, self.action_size, bidirectional=False)
+        self.linear = nn.Linear(2,1)
+
+    def forward(self,x):
+        x = x.cuda()
+        x, (_, _) = self.rnn1(x)
+        _, (h, _) = self.rnn2(x)
+        #h = tuple([h.data])
+        #x, (_, _) = self.rnn3(h)
+        #_, (h, _) = self.rnn3(x)
+        #y = x.reshape(1, x.shape[0]).clone()
+        #x = x.view(1, x.shape[0]).clone()
+        #x = torch.cat([x, torch.zeros(1, 20 - x.shape[1])], dim = 1)
+        #pad = nn.ZeroPad2d((0,15 - x.shape[1], 0, 0))
+        #x = pad(x)
+        return h.data
+
+value = ValueNetwork(1, 1, 1)
+value = value.to(device)
+
+def roll_out(actor_network,task,sample_nums,value_network,init_state):
+    #task.reset()
+    states = []
+    actions = []
+    rewards = []
+    is_done = False
+    final_r = 0
+    state = init_state
+
+
+class A2C():
+    def __init__(self):
+        super(A2C, self).__init__()
+        self.actor = ActorNetwork(1, 1, 1)
+        self.value = ValueNetwork(1, 1, 1)
+        self.value_network_optimizer = optim.RMSprop(self.value.parameters(), lr=0.0001)
+        self.actor_network_optimizer = optim.RMSprop(self.actor.parameters(), lr=0.0001)
+        self.criterion = nn.CrossEntropyLoss()
+    
+    def learn(self, obs, reward):
+        rewards.append(reward)
+        returns.append(reward + gamma * values[-1])
+        #advantage = returns[-1] - values[-1]
+
+        v = value(obs_tensor).detach()
+        values.append(v)
+
+
+
+#value_criterion = nn.CrossEntropyLoss().to(device)
+value_criterion = nn.CrossEntropyLoss().to(device)
+#value_network_optimizer = optim.RMSprop(value.parameters(), lr=1e-4)
+value_network_optimizer = optim.Adam(value.parameters(), lr=1e-4)
+
+#actor_criterion = nn.CrossEntropyLoss().to(device)
+actor_criterion = nn.CrossEntropyLoss().to(device)
+#actor_network_optimizer = optim.RMSprop(actor.parameters(), lr=1e-4)
+actor_network_optimizer = optim.Adam(actor.parameters(), lr=1e-4)
 
 try:
     while True:
@@ -53,15 +209,85 @@ try:
         print("Step: ", stepIdx)
         print("---obs:", obs)
 
+        #actor = actor.train()
+        #value = value.train()
+        obs_tensor = torch.tensor(obs, dtype=torch.float)
+        obs_tensor = torch.reshape(obs_tensor, (obs_tensor.shape[0],1))     
+
+        action = actor(obs_tensor)
+        #action = actor(obs_tensor).cpu()
+        #obs_tensor = torch.cat([obs_tensor, torch.zeros(1, 20 - obs_tensor.shape[1])], dim = 0)
+        #x = torch.cat([obs_tensor, torch.zeros(20 - obs_tensor.shape[0], 1)], dim = 0)
+        v = value(obs_tensor)
+        #v = value(obs_tensor).cpu()
+        values.append(v)
+
         while True:
             stepIdx += 1
-            action = env.action_space.sample()
-            print("---action: ", action)
 
             print("Step: ", stepIdx)
+            #obs, reward, done, info = env.step(env.action_space.sample())
+            #info of next state (can be used to calculate the predicted value of the next state) (value network)
             obs, reward, done, info = env.step(action)
-            print("---obs, reward, done, info: ", obs, reward, done, info)
+            obs_tensor = torch.tensor(obs, dtype=torch.float)
+            obs_tensor = torch.reshape(obs_tensor, (obs_tensor.shape[0],1))
 
+            '''obs_tensor = torch.empty([0], dtype = torch.float32)
+
+            for i in range(len(obs)):
+                obs_tensor = torch.cat([obs_tensor, torch.Tensor([obs[i]])])'''
+
+            rewards.append(reward)
+            #value_loss = value_criterion(torch.tensor(v, dtype=torch.float), td_target)
+
+            pred_v = values[-1]
+            v = value(obs_tensor)
+            #v = value(obs_tensor).cpu()
+            
+            print("--value: ", v)
+            #print("--shape of value: ", v.shape)
+            #advantage = reward + gamma * v - values[-1]
+            td_target = reward + gamma * v
+            td_target = torch.tensor(td_target, dtype=torch.float).requires_grad_(True)
+
+            #actor_loss = actor_criterion(values[-1], td_target)
+            actor_loss = actor_criterion(pred_v, td_target)
+            value_loss = value_criterion(pred_v, td_target)
+
+            values.append(v)
+
+            '''returns.append(values[-1])
+            for i in reversed(range(1, len(rewards))):
+                returns[i-1] = rewards[i-1] + gamma * returns[i]
+            returns = returns[:-1]'''
+            
+            '''for i in returns:
+                td_target = td_target + i'''
+
+            #advantage = returns[-1] - values[-1]
+            #td_error
+            #value_loss = actor_criterion(returns[-1], values[-1])
+            
+            #optimize actor network(based on advantage)
+            with torch.autograd.set_detect_anomaly(True):
+                actor_network_optimizer.zero_grad()
+                actor_loss.backward(retain_graph=True)
+                actor_network_optimizer.step()
+
+            #optimize actor network(based on td error)
+            with torch.autograd.set_detect_anomaly(True):
+                value_network_optimizer.zero_grad()
+                value_loss.backward()
+                value_network_optimizer.step()
+            
+            print("---obs, reward, done, info: ", obs, reward, done, info)
+    
+            action = actor(obs_tensor)
+            #action = actor(obs_tensor).cpu()
+            
+            #temp = torch.cat([obs_tensor, action],dim = 1)
+            print("---action: ", action)
+            
             if done:
                 stepIdx = 0
                 if currIt + 1 < iterationNum:
